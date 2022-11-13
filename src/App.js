853b0749a76386a7adbf81f7628f8b491131f175
unknown <kchars82@gmail.com>
2022-11-14 06:33:09 +0900

    react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MyComponet from "./MyComponent";
 
 const App = () => {
-  return <MyComponet />;
+  return <MyComponet>리액트</MyComponet>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,5 +1,15 @@
-const MyComponet = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponet = ({ name, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name} 입니다.
+      <br />
+      children 값은 {children}
+      입니다.
+    </div>
+  );
+};
+MyComponet.defaultProps = {
+  name: "기본 이름",
 };
 
 export default MyComponet;
