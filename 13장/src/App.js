02ac3289fb0bf2f5b0e0cfda5a1ec99320af9be9
KyungYunsu <kchars82@gmail.com>
2022-12-23 06:29:01 +0900

    13장 351p

--- 13장/src/App.js
+++ 13장/src/App.js
@@ -5,18 +5,20 @@ import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/profiles/:username" element={<Profile />} />
       </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
