bbfdea05ca9016b04a078b4f6d10f7d9a2de13d0
KyungYunsu <kchars82@gmail.com>
2022-12-25 22:45:00 +0900

    14장 start

---
+++ 14장/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=d9e0f2fa988644619756576669aed06b',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
