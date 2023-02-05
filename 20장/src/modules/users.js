450987b9479d348bf4bbb692c20fad040c6be1aa
KyungYunsu <kchars82@gmail.com>
2023-02-06 06:22:46 +0900

    p570 20.4.3 start

--- 20장/src/modules/users.js
+++ 20장/src/modules/users.js
@@ -5,7 +5,7 @@ const GET_USERS_SUCCESS = "users/GET_USERS_SUCCESS";
 const GET_USERS_FAILURE = "users/GET_USERS_FAILURE";
 
 const getUsersPending = () => ({ type: GET_USERS_PENDING });
-const getUsersSuccess = (payload) => ({ type: GET_USERS_SUCCESS });
+const getUsersSuccess = (payload) => ({ type: GET_USERS_SUCCESS, payload });
 const getUsersFailure = (payload) => ({
   type: GET_USERS_FAILURE,
   error: true,
@@ -41,7 +41,11 @@ const initialState = {
 function users(state = initialState, action) {
   switch (action.type) {
     case GET_USERS_PENDING:
-      return { ...state, loading: { ...state.loading, users: true } };
+      return {
+        ...state,
+        loading: { ...state.loading, users: true },
+        error: { ...state.error, users: null },
+      };
     case GET_USERS_SUCCESS:
       return {
         ...state,
